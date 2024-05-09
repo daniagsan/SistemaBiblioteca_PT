e1211093a70bf205ce6496f4b86edbc319ef77a8
JuanOrtiz17 <juandedioo_21@alu.uabcs.mx>
2024-05-09 09:42:54 -0700

    Update Main.java
    sdjkfhsdjk

--- SistemaBiblioteca/Visual/Main.java
+++ SistemaBiblioteca/Visual/Main.java
@@ -3,9 +3,7 @@ package SistemaBiblioteca.Visual;
 public class Main {
 
 	public static void main(String[] args) {
-		//Prueba GAma
-	    //que pex esto es un cambio
-
+		System.out.println("hola");
 	}
 	
 }
