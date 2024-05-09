9e6fddd5846df3ade3d8b26215799b45336d1110
Gamma <brto_22@alu.uabcs.mx>
2024-05-09 09:07:49 -0700

    Gama
    Prueba

--- SistemaBiblioteca/Visual/Main.java
+++ SistemaBiblioteca/Visual/Main.java
@@ -3,8 +3,10 @@ package SistemaBiblioteca.Visual;
 public class Main {
 
 	public static void main(String[] args) {
-		
-	    System.out.println("Hello World");
+		//Prueba GAma
+	    System.out.println("Prueba");
+		System.out.println("Prueba");
+		System.out.println("Prueba");
 
 	}
 	
