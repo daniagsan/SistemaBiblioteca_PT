6f3e84ae4cca58ad8547cc016b273babc8d301fa
Daniel Aguilera <jdas_9920@hotmail.com>
2024-05-12 23:37:21 -0700

    Update MainControl.java

--- SistemaBiblioteca/Controladores/MainControl.java
+++ SistemaBiblioteca/Controladores/MainControl.java
@@ -2,10 +2,14 @@ package SistemaBiblioteca.Controladores;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-import SistemaBiblioteca.Visual.VisualMain;
+import SistemaBiblioteca.Visual.*;
+import SistemaBiblioteca.modelos.*;
 
 public class MainControl implements ActionListener{
+
+    private ArrayList<LibroData> librosUsuario = new ArrayList<>();
 
     private VisualMain visualMain = new VisualMain();
 
